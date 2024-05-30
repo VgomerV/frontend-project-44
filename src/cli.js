f3d887c3eb3869898b1d2f478d92093ef75dddce
Gomer <vgomer@list.ru>
2024-05-31 00:10:13 +0300

    task 3 completed

--- src/cli.js
+++ src/cli.js
@@ -1,5 +1,7 @@
+import readlineSync from 'readline-sync';
+
 export const greet = () => {
 console.log('Welcome to the Brain Games!');
-const userName = readlineSync.question('May I have your name?');
+const userName = readlineSync.question('May I have your name? ');
 console.log(`Hello, ${userName}`);
 }
