3193c436e5b56062d474f55ddc00a1763376166e
Vitaly Gomer <vgomer@list.ru>
2025-09-08 06:19:10 +0300

    Add GCD game

---
+++ src/games/gcd.js
@@ -0,0 +1,29 @@
+import gameCore, { getRandomInt } from '../index.js'
+
+const gameRules = 'Find the greatest common divisor of given numbers.'
+const maxRandomInt = 100
+
+const findGCD = (firstNum, secondNum) => {
+  let dividend = firstNum > secondNum ? firstNum : secondNum
+  let divisor = firstNum < secondNum ? firstNum : secondNum
+
+  while (divisor > 0) {
+    const remainder = dividend % divisor
+    dividend = divisor
+    divisor = remainder
+  }
+
+  return dividend.toString()
+}
+
+const evenGameLogic = () => {
+  const randomNum1 = getRandomInt(0, maxRandomInt)
+  const randomNum2 = getRandomInt(0, maxRandomInt)
+
+  const question = `${randomNum1} ${randomNum2}`
+  const correctAnswer = findGCD(randomNum1, randomNum2)
+
+  return [question, correctAnswer]
+}
+
+export default () => gameCore(gameRules, evenGameLogic)
